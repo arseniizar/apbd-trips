f9ddd847fc789d0984c77db9f79164a18fc7f7b1
Dmytro Borovych <dmitryborovich@gmail.com>
2024-05-21 03:03:09 +0200

    Implement solution example with multiple projects

---
+++ TripApp/TripApp.Core/Repository/ITripRepository.cs
@@ -0,0 +1,9 @@
+﻿using TripApp.Core.Model;
+
+namespace TripApp.Core.Repository;
+
+public interface ITripRepository
+{
+    Task<PaginatedResult<Model.Trip>> GetPaginatedTripsAsync(int page = 1, int pageSize = 10);
+    Task<List<Model.Trip>> GetAllTripsAsync();
+}
